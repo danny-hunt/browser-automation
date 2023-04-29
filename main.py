ee04e6a69706bed93760373a72f10aaa3a88105f
Danny Hunt <dansinh64@gmail.com>
2023-04-29 14:10:40 +0100

    tidying and add audio transcription

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ def prompt(user_message: str):
     return f"""
 Please respond with only javascript code that can be executed in the browser console that will fulfil this objective:
 {user_message}
+The code should not do anything other than fulfil that objective.
 Do not respond with anything other than code. There should be no explanation of the code. Any text that is not valid javascript code will be rejected.
 The username is {username} and the password is {password}.
 The current page in html is the following:"""
@@ -25,22 +26,6 @@ login_prompt = f"I want to login. My username is {username} and my password is {
 
 scroll_prompt = "I want to scroll to the bottom of the page."
 
-def query(user_message: str, html: Optional[str]):
-    return openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system", 
-            "content": prompt(user_message)
-        }, 
-        {
-            "role": "user",
-            "content": str(html)
-        }
-    ],
-    temperature=0,
-    max_tokens=150,
-)
 
 def process_output(output_message: str) -> str:
     message = output_message.replace("\\", "")
@@ -50,29 +35,54 @@ def process_output(output_message: str) -> str:
         message = message.split("`")[1]
     return message.strip()
 
+def query(user_message: str, html: Optional[str]):
+    print("===")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system", 
+                "content": prompt(user_message)
+            }, 
+            {
+                "role": "user",
+                "content": str(html)
+            }
+        ],
+        temperature=0,
+        max_tokens=150,
+    )
+    print(process_output(response["choices"][0]["message"]["content"]))
 
-login_response = query(login_prompt, hackernews_login_html)
-print(process_output(login_response["choices"][0]["message"]["content"]))
 
-print ("===")
+audio_file = "./audio_files/submit.m4a"
+def voice_to_text(file_name: str) -> str:
+    print("=== voice attempt now ===")
+    with open(file_name, "rb") as audio:
+        transcript = openai.Audio.transcribe("whisper-1", audio)["text"]
+    print(f'Audio clip transcribed as: "{transcript}"')
+    return transcript
 
-scroll = query(scroll_prompt, None)
-print(process_output(scroll["choices"][0]["message"]["content"]))
 
-print ("===")
-go_to_post_creation = query("I want to go to the post creation page.", hackernews_home_html)
-print(process_output(go_to_post_creation["choices"][0]["message"]["content"]))
+# """
+query(f"I want to login. My username is {username} and my password is {password}.", hackernews_login_html)
 
-print ("===")
-create_post = query(
+query("I want to scroll to the bottom of the page.", None)
+
+query("I want to go to the post creation page.", hackernews_home_html)
+
+query(
     "I want to fill in the title and description fields. I want to tell the readers of hackernews that this post is the culmanation of my work at a hackathon in London.", 
     hackernew_submit_html
 )
-print(process_output(create_post["choices"][0]["message"]["content"]))
 
-print ("===")
-submit_post = query(
+query(
     "I want to click the submit button", # This prompt at the moment has to be super specific since we don't update the html after the previous step
     hackernew_submit_html
 )
-print(process_output(submit_post["choices"][0]["message"]["content"]))
+
+query(
+    voice_to_text(audio_file),
+    hackernew_submit_html
+)
+
